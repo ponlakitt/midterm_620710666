324ed65d9dedafc2d150294c88dbf6f66b71598e
Ponlakit Konkrathok <Ponlakit13@hotmail.com>
2022-01-16 12:18:33 +0700

    midterm

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'GUESS THE NUMBER',
       theme: ThemeData(
-        primarySwatch: Colors.orange,
+        primarySwatch: Colors.pink,
       ),
       home: const HomePage(),
     );
@@ -25,48 +25,75 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('GUESS THE NUMBER')),
       body: Container(
+        constraints: BoxConstraints.expand(),
         decoration: BoxDecoration(
-            color: Colors.orange.shade100,
-            border: Border.all(width: 5.0, color: Colors.orange.shade100),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.white.withOpacity(0.5),
-                offset: const Offset(2.0, 5.0),
-                blurRadius: 5.0,
-                spreadRadius: 2.0,
-              ),
-            ]),
+          image: DecorationImage(
+            image: AssetImage('/images/bg.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+
         //alignment: Alignment.center,
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset('assets/images/guess_logo.png', width: 100.0),
-                  Column(
+          child: Padding(
+            padding: const EdgeInsets.all(60.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text('PIG WEIGHT',style:TextStyle(fontSize: 30,color: Colors.pink),),
+                        Text('CALCULATOR',style:TextStyle(fontSize: 30,color: Colors.pink),),
+                      ],)
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(40.0),
+                  child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Text(' GUESS',style:TextStyle(fontSize: 40,color: Colors.black),),
-                      Text(' THE NUMBER',style:TextStyle(fontSize: 20,color: Colors.black),),
-                    ],)
-                ],
-              ),
-              SizedBox(height: 40,),
-              Padding(
-                padding: const EdgeInsets.all(40.0),
-                child: TextField(),
-              ),
-              ElevatedButton(
-                child: Text('GUESS'),
-                onPressed: () {
-                  // โค้ดที่จะทำงานเมื่อกดปุ่ม
-                },
-              ),
-            ],
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Image.asset('assets/images/pig.png', width: 280.0),
+                        ],)
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+
+                      ],)
+                  ],
+                ),
+                SizedBox(height: 200,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ElevatedButton(
+
+                          child: Text('CALCULAT'),
+                          onPressed: () {
+                            // โค้ดที่จะทำงานเมื่อกดปุ่ม
+                          },
+                        ),
+                      ],)
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
